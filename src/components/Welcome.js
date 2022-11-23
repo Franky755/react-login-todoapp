9af73472a34eb33c4e09e4179b2c62c61918c45a
Franky755 <mittoknight@gmail.com>
2022-11-23 17:42:27 +0700

    update task 3

---
+++ src/components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputForm from './InputForm';
+
+
+const Welcome = (props, ) => {
+  return (
+    <div>
+     Welcome, <a href=''>{props.name}</a>!
+    </div>
+  )
+}
+
+export default Welcome
